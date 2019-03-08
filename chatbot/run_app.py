a81c852ce5c8f4a608e57d15d1bc4f44b7a326a6
Marlon Enrique Noguera Ramirez <36613466+menoguerar@users.noreply.github.com>
2019-03-07 20:52:36 -0500

    Update run_app.py

--- chatbot/run_app.py
+++ chatbot/run_app.py
@@ -9,7 +9,7 @@ nlu_interpreter = RasaNLUInterpreter('./models/nlu/default/weathernlu')
 action_endpoint = EndpointConfig(url="http://localhost:5055/webhook")
 agent = Agent.load('./models/dialogue', interpreter = nlu_interpreter, action_endpoint = action_endpoint)
 
-input_channel = SlackInput('xoxb-562135625030-560134188337-KuGSywOoV6O0BNm2GZEsVzrO' #your bot user authentication token
+input_channel = SlackInput('' #your bot user authentication token
                            )
 
 agent.handle_channels([input_channel], 5004, serve_forever=True)
